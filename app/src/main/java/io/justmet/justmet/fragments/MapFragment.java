1eb4b99e1af9cb676b14eb03d6c69a8de5ab5210
Malcolm <mmnavarr@live.com>
2017-01-07 17:38:47 -0500

    Fragments refactor.

--- app/src/main/java/io/justmet/justmet/fragments/MapFragment.java
+++ app/src/main/java/io/justmet/justmet/fragments/MapFragment.java
@@ -23,6 +23,6 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        return inflater.inflate(R.layout.fragment_map, container, false);
     }
 }
--- app/src/main/java/io/justmet/justmet/fragments/ProfileFragment.java
+++ app/src/main/java/io/justmet/justmet/fragments/ProfileFragment.java
@@ -23,6 +23,6 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
