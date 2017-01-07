dc72d383ca2478412fb2b2ab5bcce40693f7f3c5
Matt Kujawinski <mattkuj@yahoo.com>
2017-01-07 14:00:43 -0500

    - Added fragments  - Added color scheme

--- app/src/main/java/io/justmet/justmet/fragments/MapFragment.java
+++ app/src/main/java/io/justmet/justmet/fragments/MapFragment.java
@@ -1,17 +1,19 @@
-package io.justmet.justmet;
+package io.justmet.justmet.fragments;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import io.justmet.justmet.R;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MapFragment extends Fragment {
 
-    public MainActivityFragment() {
+    public MapFragment() {
     }
 
     @Override
