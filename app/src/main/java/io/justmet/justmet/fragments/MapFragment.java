80acd5dbb0e03cc23f3205cfa58ca262bc2d1d6a
Matt Kujawinski <mattkuj@yahoo.com>
2017-01-09 23:08:27 -0500

    Got google maps (sorta) working

--- app/src/main/java/io/justmet/justmet/fragments/MapFragment.java
+++ app/src/main/java/io/justmet/justmet/fragments/MapFragment.java
@@ -6,23 +6,66 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import io.justmet.justmet.R;
 
 
 public class MapFragment extends Fragment {
 
-    public MapFragment() {
-
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private MapView mapView;
+    private GoogleMap gMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+        // Initialize a view to inflate
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        // Get the MapView from the XML layout and create it
+        mapView = (MapView) view.findViewById(R.id.map_frag);
+        mapView.onCreate(savedInstanceState);
+
+        // Get to GoogleMap from the MapView and add marker at Empire State Bld
+        gMap = mapView.getMap();
+        gMap.addMarker(new MarkerOptions().position(new LatLng(40.7484,73.5897)));
+
+        // Call MapsInitializer before calling CameraUpdateFactory
+        try {
+            MapsInitializer.initialize(this.getActivity());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        // Update the location and zoom of the MapView
+        CameraUpdate cam = CameraUpdateFactory.newLatLngZoom(new LatLng(41,74),10);
+        gMap.animateCamera(cam);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
